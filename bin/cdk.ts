aa15501677c4e4092d4c4be088e9bad3f4cd3f95
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2020-07-17 13:47:04 -0400

    LW-1013

---
+++ bin/cdk.ts
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+import 'source-map-support/register'
+import { execSync } from 'child_process'
+import * as cdk from '@aws-cdk/core'
+import { StackTags } from '@ndlib/ndlib-cdk'
+import fs = require('fs')
+import IlliadGatewayStack from '../src/illiad-gateway-stack'
+import IlliadGatewayPipelineStack from '../src/illiad-gateway-pipeline-stack'
+
+// The context values here are defaults only. Passing context in cli will override these
+const username = execSync('id -un').toString().trim()
+const app = new cdk.App({
+  context: {
+    owner: username,
+    contact: `${username}@nd.edu`,
+  },
+})
+app.node.applyAspect(new StackTags())
+
+const stage = app.node.tryGetContext('stage') || 'dev'
+const sentryProject = app.node.tryGetContext('sentryProject')
+const secretsPath = app.node.tryGetContext('secretsPath')
+
+let lambdaCodePath = app.node.tryGetContext('lambdaCodePath')
+let sentryVersion = app.node.tryGetContext('sentryVersion')
+if (!lambdaCodePath && fs.existsSync('../illiad-gateway')) {
+  lambdaCodePath = '../illiad-gateway/src'
+  sentryVersion = execSync(`cd ${lambdaCodePath} && git rev-parse HEAD`).toString().trim()
+}
+
+if (lambdaCodePath) {
+  const stackName = app.node.tryGetContext('serviceStackName') || `illiad-gateway-${stage}`
+  new IlliadGatewayStack(app, stackName, {
+    stackName,
+    description: `API service that stands between other apps/services and Illiad's APIs.`,
+    stage,
+    lambdaCodePath,
+    sentryProject,
+    sentryVersion,
+    secretsPath,
+  })
+}
+
+const pipelineName = app.node.tryGetContext('pipelineStackName') || `illiad-gateway-pipeline`
+new IlliadGatewayPipelineStack(app, pipelineName, {
+  stackName: pipelineName,
+  gitOwner: app.node.tryGetContext('gitOwner'),
+  gitTokenPath: app.node.tryGetContext('gitTokenPath'),
+  serviceRepository: app.node.tryGetContext('serviceRepository'),
+  serviceBranch: app.node.tryGetContext('serviceBranch'),
+  blueprintsRepository: app.node.tryGetContext('blueprintsRepository'),
+  blueprintsBranch: app.node.tryGetContext('blueprintsBranch'),
+  contact: app.node.tryGetContext('contact'),
+  owner: app.node.tryGetContext('owner'),
+  sentryTokenPath: app.node.tryGetContext('sentryTokenPath'),
+  sentryOrg: app.node.tryGetContext('sentryOrg'),
+  sentryProject,
+  emailReceivers: app.node.tryGetContext('emailReceivers'),
+  secretsPath,
+})
---
+++ src/illiad-gateway-stack.ts
@@ -0,0 +1,141 @@
+import * as cdk from '@aws-cdk/core'
+import { SecretValue } from '@aws-cdk/core'
+import apigateway = require('@aws-cdk/aws-apigateway')
+import lambda = require('@aws-cdk/aws-lambda')
+import { RetentionDays } from '@aws-cdk/aws-logs'
+import { StringParameter } from '@aws-cdk/aws-ssm'
+
+export interface IIlliadGatewayStackProps extends cdk.StackProps {
+  readonly stage: string
+  readonly lambdaCodePath: string
+  readonly sentryProject: string
+  readonly sentryVersion: string
+  readonly secretsPath: string
+}
+
+export default class IlliadGatewayStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props: IIlliadGatewayStackProps) {
+    super(scope, id, props)
+
+    // LAMBDAS
+    const paramStorePath = `/all/illiad-gateway/${props.stage}`
+    const env = {
+      SENTRY_DSN: StringParameter.valueForStringParameter(this, `${paramStorePath}/sentry_dsn`),
+      SENTRY_ENVIRONMENT: props.stage,
+      SENTRY_RELEASE: `${props.sentryProject}@${props.sentryVersion}`,
+      ILLIAD_URL: StringParameter.valueForStringParameter(this, `${paramStorePath}/illiad_url`),
+      API_KEY: SecretValue.secretsManager(props.secretsPath, { jsonField: 'api_key' }).toString(),
+    }
+
+    const allLambda = new lambda.Function(this, 'AllFunction', {
+      functionName: `${props.stackName}-all`,
+      description: 'Get all non-cancelled requests for a user by netid.',
+      code: lambda.Code.fromAsset(props.lambdaCodePath),
+      handler: 'all.handler',
+      runtime: lambda.Runtime.NODEJS_12_X,
+      logRetention: RetentionDays.ONE_WEEK,
+      memorySize: 128,
+      timeout: cdk.Duration.seconds(30),
+      environment: env,
+    })
+
+    const borrowedLambda = new lambda.Function(this, 'BorrowedFunction', {
+      functionName: `${props.stackName}-borrowed`,
+      description: 'Get both physical and digital items loaned to a user by netid.',
+      code: lambda.Code.fromAsset(props.lambdaCodePath),
+      handler: 'borrowed.handler',
+      runtime: lambda.Runtime.NODEJS_12_X,
+      logRetention: RetentionDays.ONE_WEEK,
+      memorySize: 128,
+      timeout: cdk.Duration.seconds(30),
+      environment: env,
+    })
+
+    const checkedOutLambda = new lambda.Function(this, 'CheckedOutFunction', {
+      functionName: `${props.stackName}-checkedOut`,
+      description: 'Get physical items loaned to a user by netid.',
+      code: lambda.Code.fromAsset(props.lambdaCodePath),
+      handler: 'checkedOut.handler',
+      runtime: lambda.Runtime.NODEJS_12_X,
+      logRetention: RetentionDays.ONE_WEEK,
+      memorySize: 128,
+      timeout: cdk.Duration.seconds(30),
+      environment: env,
+    })
+
+    const webLambda = new lambda.Function(this, 'WebFunction', {
+      functionName: `${props.stackName}-web`,
+      description: 'Get digital items loaned to a user by netid.',
+      code: lambda.Code.fromAsset(props.lambdaCodePath),
+      handler: 'web.handler',
+      runtime: lambda.Runtime.NODEJS_12_X,
+      logRetention: RetentionDays.ONE_WEEK,
+      memorySize: 128,
+      timeout: cdk.Duration.seconds(30),
+      environment: env,
+    })
+
+    const pendingLambda = new lambda.Function(this, 'PendingFunction', {
+      functionName: `${props.stackName}-pending`,
+      description: 'Get pending request that have not been fulfilled or cancelled for a user by netid.',
+      code: lambda.Code.fromAsset(props.lambdaCodePath),
+      handler: 'pending.handler',
+      runtime: lambda.Runtime.NODEJS_12_X,
+      logRetention: RetentionDays.ONE_WEEK,
+      memorySize: 128,
+      timeout: cdk.Duration.seconds(30),
+      environment: env,
+    })
+
+    // API GATEWAY
+    const api = new apigateway.RestApi(this, 'ApiGateway', {
+      restApiName: props.stackName,
+      description: 'Illiad Gateway API',
+      endpointExportName: `${props.stackName}-api-url`,
+      deployOptions: {
+        stageName: props.stage,
+        metricsEnabled: true,
+        loggingLevel: apigateway.MethodLoggingLevel.ERROR,
+      },
+      defaultCorsPreflightOptions: {
+        allowOrigins: ['*'],
+        allowCredentials: false,
+        statusCode: 200,
+      },
+    })
+    api.addRequestValidator('RequestValidator', {
+      validateRequestParameters: true,
+    })
+
+    const defaultMethodOptions = {
+      authorizationType: apigateway.AuthorizationType.CUSTOM,
+      authorizer: new apigateway.TokenAuthorizer(this, 'JwtAuthorizer', {
+        handler: lambda.Function.fromFunctionArn(
+          this,
+          'AuthorizerFunction',
+          `arn:aws:lambda:${this.region}:${this.account}:function:lambda-auth-${props.stage}`,
+        ),
+        identitySource: 'method.request.header.Authorization',
+        authorizerName: 'jwt',
+        resultsCacheTtl: cdk.Duration.minutes(5),
+      }),
+      requestParameters: {
+        'method.request.header.Authorization': true,
+      },
+    }
+
+    const endpointData = [
+      { path: 'all', lambda: allLambda },
+      { path: 'borrowed', lambda: borrowedLambda },
+      { path: 'checkedOut', lambda: checkedOutLambda },
+      { path: 'web', lambda: webLambda },
+      { path: 'pending', lambda: pendingLambda },
+    ]
+    endpointData.forEach(endpoint => {
+      const newResource = api.root.addResource(endpoint.path, {
+        defaultMethodOptions: defaultMethodOptions,
+      })
+      newResource.addMethod('GET', new apigateway.LambdaIntegration(endpoint.lambda))
+    })
+  }
+}
